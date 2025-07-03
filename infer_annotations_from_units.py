69f61c920180848ac5d02dc9931a798dbe3c61e4
David Nickerson <david.nickerson@gmail.com>
2025-07-03 16:39:36 +1200

    initial test at finding variables with 'volume' units

--- infer_annotations_from_units.py
+++ infer_annotations_from_units.py
@@ -1,4 +1,5 @@
 import sys
+import libcellml
 from libcellml_python_utils import cellml
 
 
@@ -6,3 +7,22 @@ model = cellml.parse_model(sys.argv[1], False)
 if cellml.validate_model(model) > 0:
     exit(-1)
 
+# Create a reference "volume" units, e.g. litre or m^3
+volume_units = libcellml.Units()
+volume_units.addUnit("metre", 1, 3.0)
+
+# Find volume-equivalent variables
+volume_variables = []
+for i in range(model.componentCount()):
+    comp = model.component(i)
+    for j in range(comp.variableCount()):
+        var = comp.variable(j)
+        var_units_name = var.units().name()
+        var_units = model.units(var_units_name)
+        if var_units and libcellml.Units.equivalent(var_units, volume_units):
+            volume_variables.append((comp.name(), var.name(), var_units_name))
+
+# Output
+print("Variables with volume-equivalent units:")
+for comp_name, var_name, units in volume_variables:
+    print(f"Component: {comp_name}, Variable: {var_name}, Units: {units}")
